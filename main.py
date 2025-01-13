c64c7d9c2e3ab61c81eb3d1dd32e2615e3cebe65
Ansh Paswan <anshpaswan1983@gmail.com>
2025-01-13 17:56:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ out_time_responses = {}
 employee_reminder_times = {}  # Track the time when the last reminder was sent
 
 # Employee phone numbers
-EMPLOYEES = ['whatsapp:+918178486210','whatsapp:+918800177870']
+EMPLOYEES = ['whatsapp:mobile1','whatsapp:+mobile2']
 
 # Function to log data to CSV
 def log_to_csv(phone_number, date, in_time="", out_time="", present="", leave="", leave_reason=""):
